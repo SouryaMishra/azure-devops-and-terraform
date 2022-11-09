dd155607c29dbdd6060a05a5d6d98c7a4eeadf89
Sourya Mishra <sourya.mishra111@gmail.com>
2022-11-10 01:30:02 +0530

    add main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "resource-group"
+  location = "South India"
+}
+
+resource "azurerm_container_group" "cg" {
+  name                = "weatherapi"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_address_type = "Public"
+  dns_name_label  = "souryamishra-weather-api"
+  os_type         = "Linux"
+
+  container {
+    name   = "weatherapi"
+    image  = "souryamishra/weatherapi"
+    cpu    = "1"
+    memory = "1"
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
